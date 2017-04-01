2c43ed44045450bdb1d324c312d2748fbc79d3f3
robs <rob.swartenbroekx@cegeka.com>
2017-04-01 09:16:22 +0200

    Added GameOfLifeController

---
+++ GameOfLifeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace gameoflifecore
+{
+    class GameOfLifeController
+    {
+        List<List<bool>> GetWorld(List<List<bool>> currentWorld)
+        {
+            return null;
+        }
+    }
+}
