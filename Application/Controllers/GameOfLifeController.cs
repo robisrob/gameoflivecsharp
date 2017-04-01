3d2f468ede0e523edc7c556756ea3282a3f7b3e1
robs <rob.swartenbroekx@cegeka.com>
2017-04-01 15:25:21 +0200

    Added restapi

---
+++ Application/Controllers/GameOfLifeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GameOfLife.Application;
+
+
+namespace Application.Controllers
+{
+    [Route("api/[controller]")]
+    public class GameOfLifeController : Controller
+    {
+        [HttpPost]
+        public List<List<bool>> GetWorld([FromBody]List<List<bool>> currentWorld)
+        {
+            var a = new List<List<bool>> {new List<bool>{true, false}};
+            return new GameOfLife.Application.GameOfLifeController().GetWorld(currentWorld);
+       }
+    }
+}
