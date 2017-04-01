a9df4f2ebcd192f8c4433354fdf8dff73fbd0bbd
robs <rob.swartenbroekx@cegeka.com>
2017-04-01 08:57:21 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gameoflifecore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
