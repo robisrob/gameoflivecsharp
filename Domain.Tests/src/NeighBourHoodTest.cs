78fbdb5c0427f807469b3925a150be792a43bfb0
robs <rob.swartenbroekx@cegeka.com>
2017-04-16 08:54:30 +0200

    refactored neighborhoodtest to better express the rules

--- Domain.Tests/src/NeighBourHoodTest.cs
+++ Domain.Tests/src/NeighBourHoodTest.cs
@@ -6,65 +6,43 @@ namespace GameOfLife.Domain.NeighbourHood
 {
 public class NeighBourHoodTest {
 
-    [Fact]
-    public void GivenZeroLivingNeighborAndCelIsDead_WhenDetermineNextStatus_ThenNextStatusIsDead() {
-        new Neighbourhood(0).DetermineNextStatus(Cel.Dead).Should().Be(Cel.Dead);
+    [Theory]
+    [InlineData(0, Cel.Dead)]
+    [InlineData(0, Cel.Alive)]
+    [InlineData(1, Cel.Dead)]
+    [InlineData(1, Cel.Alive)]
+
+    public void GivenLessThenTwoLivingNeigbors_WhenDetermineNewGeneration_ThenNewCelIsDead_DueToUnderPopulation(int livingNeighbors, Cel currentCel) {
+        new Neighbourhood(livingNeighbors).DetermineNextStatus(currentCel).Should().Be(Cel.Dead);
     }
 
-    [Fact]
-    public void GivenZeroLivingNeighborAndCelIsLiving_WhenDetermineNextStatus_ThenNextStatusIsDead() {
-        new Neighbourhood(0).DetermineNextStatus(Cel.Alive).Should().Be(Cel.Dead);
+    [Theory]
+    [InlineData(Cel.Dead)]
+    [InlineData(Cel.Alive)]
+    public void GivenTwoLivingNeigbors_WhenDetermineNewGeneration_ThenNewCelIsTheSameAsOldCel_DueToSustenance(Cel currentCel) {
+        new Neighbourhood(2).DetermineNextStatus(currentCel).Should().Be(currentCel);
     }
 
-    [Fact]
-    public void GivenOneLivingNeighborAndCelIsDead_WhenDetermineNextStatus_ThenNextStatusIsDead() {
-        new Neighbourhood(1).DetermineNextStatus(Cel.Dead).Should().Be(Cel.Dead);
+    [Theory]
+    [InlineData(Cel.Dead)]
+    [InlineData(Cel.Alive)]
+    public void GivenThreeLivingNeigbors_WhenDetermineNewGeneration_ThenNewCelIsAlive_DueToReproduction(Cel currentCel) {
+        new Neighbourhood(3).DetermineNextStatus(currentCel).Should().Be(Cel.Alive);
     }
 
-    [Fact]
-    public void GivenOneLivingNeighborAndCelIsLiving_WhenDetermineNextStatus_ThenNextStatusIsDead() {
-        new Neighbourhood(1).DetermineNextStatus(Cel.Alive).Should().Be(Cel.Dead);
-    }
-
-    [Fact]
-    public void GivenTwoLivingNeighborAndCelIsDead_WhenDetermineNextStatus_ThenNextStatusIsDead() {
-        new Neighbourhood(2).DetermineNextStatus(Cel.Dead).Should().Be(Cel.Dead);
-    }
-
-    [Fact]
-    public void GivenTwoLivingNeighborAndCelIsLiving_WhenDetermineNextStatus_ThenNextStatusIsLiving() {
-        new Neighbourhood(2).DetermineNextStatus(Cel.Alive).Should().Be(Cel.Alive);
-    }
-
-    [Fact]
-    public void GivenThreeLivingNeighborAndCelIsDead_WhenDetermineNextStatus_ThenNextStatusIsDead() {
-        new Neighbourhood(3).DetermineNextStatus(Cel.Dead).Should().Be(Cel.Alive);
-    }
-
-    [Fact]
-    public void GivenThreeLivingNeighborAndCelIsLiving_WhenDetermineNextStatus_ThenNextStatusIsDead() {
-        new Neighbourhood(3).DetermineNextStatus(Cel.Alive).Should().Be(Cel.Alive);
-    }
-
-    [Fact]
-    public void GivenFourLivingNeighborAndCelIsDead_WhenDetermineNextStatus_ThenNextStatusIsDead() {
-        new Neighbourhood(4).DetermineNextStatus(Cel.Dead).Should().Be(Cel.Dead);
-    }
-
-    [Fact]
-    public void GivenFourLivingNeighborAndCelIsLiving_WhenDetermineNextStatus_ThenNextStatusIsDead() {
-        new Neighbourhood(4).DetermineNextStatus(Cel.Alive).Should().Be(Cel.Dead);
-    }
-
-    [Fact]
-
-    public void GivenEightLivingNeighborAndCelIsDead_WhenDetermineNextStatus_ThenNextStatusIsDead() {
-        new Neighbourhood(8).DetermineNextStatus(Cel.Dead).Should().Be(Cel.Dead);
-    }
-
-    [Fact]
-    public void GivenEightLivingNeighborAndCelIsLiving_WhenDetermineNextStatus_ThenNextStatusIsDead() {
-        new Neighbourhood(8).DetermineNextStatus(Cel.Alive).Should().Be(Cel.Dead);
+    [Theory]
+    [InlineData(4, Cel.Dead)]
+    [InlineData(4, Cel.Alive)]
+    [InlineData(5, Cel.Dead)]
+    [InlineData(5, Cel.Alive)]
+    [InlineData(6, Cel.Dead)]
+    [InlineData(6, Cel.Alive)]
+    [InlineData(7, Cel.Dead)]
+    [InlineData(7, Cel.Alive)]
+    [InlineData(8, Cel.Dead)]
+    [InlineData(8, Cel.Alive)]
+    public void GivenMoreThenThreeLivingNeighbors_WhenDetermineNewGeneration_ThenNewCelDead_DueToOverpopulation(int amountOfLivingNeighbors, Cel currentCel) {
+        new Neighbourhood(amountOfLivingNeighbors).DetermineNextStatus(currentCel).Should().Be(Cel.Dead);
     }
 
     [Fact]
