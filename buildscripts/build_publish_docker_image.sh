18e1c574f34589f4a137da2a7b9bf2d11b85ad5c
Rob Swartenbroekx <rob.swartenbroekx@gmail.com>
2017-05-28 15:14:45 +0200

    Trying to fix build

--- buildscripts/build_publish_docker_image.sh
+++ buildscripts/build_publish_docker_image.sh
@@ -3,7 +3,7 @@ dotnet publish -c Release -o ./docker_workdir/app
 cp ../buildscripts/Dockerfile ./docker_workdir
 cd docker_workdir
 docker build -t robisrob/gol .
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker login -u=${DOCKER_USERNAME} -p=${DOCKER_PASSWORD}
 docker push robisrob/gol
 
 
