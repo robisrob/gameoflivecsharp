22a73790f14717c77bf593b8e774a10ec7ac69d4
robs <rob.swartenbroekx@cegeka.com>
2017-05-28 23:18:49 +0200

    remove print statements in deploy script

--- buildscripts/build_publish_docker_image.sh
+++ buildscripts/build_publish_docker_image.sh
@@ -3,7 +3,6 @@ dotnet publish -c Release -o ./docker_workdir/app
 cp ../buildscripts/Dockerfile ./docker_workdir
 cd docker_workdir
 docker build -t robisrob/gol .
-echo "${DOCKER_USERNAME}"
 docker login -u="${DOCKER_USERNAME}" -p="${DOCKER_PASSWORD}"
 docker push robisrob/gol
 
