29d92d893b5fb31f8e4d8a8f46ab85792603b00f
robs <rob.swartenbroekx@cegeka.com>
2017-05-28 13:33:36 +0200

    trying to fix build

--- buildscripts/build_publish_docker_image.sh
+++ buildscripts/build_publish_docker_image.sh
@@ -1,7 +1,8 @@
 cd $HOME
-dotnet publish -c Release -o ./docker_workdir/app ./Application.csproj
-cp buildscripts/Dockerfile Application/docker_workdir
-cd Application/docker_workdir
+cd Application
+dotnet publish -c Release -o ./docker_workdir/app 
+cp ../buildscripts/Dockerfile ./docker_workdir
+cd docker_workdir
 docker build -t robisrob/gol .
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 docker push robisrob/gol
