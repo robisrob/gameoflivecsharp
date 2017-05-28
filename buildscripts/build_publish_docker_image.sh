4a0d6c56400e56ac02954fef6cf781e62b6032c2
robs <rob.swartenbroekx@cegeka.com>
2017-05-28 13:17:56 +0200

    added build and publish docker image step in travis

---
+++ buildscripts/build_publish_docker_image.sh
@@ -0,0 +1,9 @@
+cd Application
+dotnet publish -c Release -o ./docker_workdir/app ./Application/Application.csproj
+cp buildscripts/Dockerfile Application/docker_workdir
+cd Application/docker_workdir
+docker build -t robisrob/gol .
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker push robisrob/gol
+
+
